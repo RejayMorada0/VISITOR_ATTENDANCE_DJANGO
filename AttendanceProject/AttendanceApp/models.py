8c332a3cf212608363e1282a8812d6b25021c460
RejayMorada0 <92773912+RejayMorada0@users.noreply.github.com>
2023-06-17 10:15:36 +0800

    Update models.py

--- AttendanceProject/AttendanceApp/models.py
+++ AttendanceProject/AttendanceApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import Model
+import os
 
-# Create your models here.
+class Visitors(models.Model):
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    contact_number = models.IntegerField(null=True, blank=True)
+    person_to_visit = models.ForeignKey("Staffs", on_delete=models.CASCADE)
+    purpose = models.CharField(max_length=500, null=True, blank=True)
+    picture = models.FileField(upload_to ='pictures/', null=True, blank=True)
+  
+    class Meta:
+        verbose_name_plural = "Visitors" 
+
+
+class Staffs(models.Model):
+    staff_name = models.CharField(max_length=255, null=True, blank=True)
+    which_department = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Staffs" 
