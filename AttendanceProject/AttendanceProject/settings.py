e9044ca5bbf37c5ee31a755721576ae933ab9057
RejayMorada0 <92773912+RejayMorada0@users.noreply.github.com>
2023-06-17 10:45:05 +0800

    Update settings.py

--- AttendanceProject/AttendanceProject/settings.py
+++ AttendanceProject/AttendanceProject/settings.py
@@ -73,12 +73,16 @@ WSGI_APPLICATION = 'AttendanceProject.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'visitors_db',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
     }
 }
 
