4a01130a77439d3650c4919b2776170016b851c9
RejayMorada0 <92773912+RejayMorada0@users.noreply.github.com>
2023-06-17 10:39:15 +0800

    Update models.py

--- AttendanceProject/AttendanceApp/models.py
+++ AttendanceProject/AttendanceApp/models.py
@@ -11,6 +11,12 @@ class Visitors(models.Model):
     purpose = models.CharField(max_length=500, null=True, blank=True)
     picture = models.FileField(upload_to ='pictures/', null=True, blank=True)
     status = models.CharField(max_length=255, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.rfid:
+            # Generate RFID value if not already set
+            self.rfid = generate_rfid()
+        super().save(*args, **kwargs)
     
     class Meta:
         verbose_name_plural = "Visitors" 
