5776694403efd688c95b867308dd4b7ee9ff45c1
CyrineOsorio <cyrineosorio18@gmail.com>
2023-06-17 12:31:18 +0800

    Add Files

--- AttendanceProject/AttendanceApp/views.py
+++ AttendanceProject/AttendanceApp/views.py
@@ -6,6 +6,7 @@ from .models import *
 
 import csv
 import qrcode
+import uuid
 import os
 import time
 from datetime import datetime
@@ -17,21 +18,47 @@ from django.db.models import Q, Count, Sum
 installed_apps = ['AttendanceApp']
 
 
-def generate_rfid():
-    return str(uuid.uuid4())[:8]  # Generate a unique RFID value (e.g., first 8 characters of a UUID)
+# def generate_rfid():
+#     return str(uuid.uuid4())[:8]  
 
 def visitor(request):
+    staff = Staffs.objects.all()
     if request.method=='POST':
+        rfid = str(uuid.uuid4())[:8] # Generate a unique RFID value (e.g., first 8 characters of a UUID)
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         contact_number  = request.POST.get('contact_number')
         person_to_visit = request.POST.get('person_to_visit')
         purpose = request.POST.get('purpose')
         picture = request.FILES["picture"]
-        data = Visitors.objects.create(first_name = first_name, last_name = last_name, contact_number = contact_number, person_to_visit = person_to_visit, purpose = purpose, picture = picture, status = '')
+        data = Visitors.objects.create(rfid = rfid, first_name = first_name, last_name = last_name, contact_number = contact_number, person_to_visit_id = person_to_visit, purpose = purpose, picture = picture, status = '')
         data.save()
+        # Generate QR COde
+        def generate_qr_code(data, output_file):
+            # Create a QR code instance
+            qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+
+            # Add data to the QR code
+            qr.add_data(data)
+            qr.make(fit=True)
+
+            # Create an image from the QR code
+            qr_image = qr.make_image(fill_color="black", back_color="white")
+
+            # Save the image to a file
+            qr_image.save(output_file)
+
+        # Usage example
+        my_data = rfid  # Replace with your variable or data
+        output_file = "qr_code.png"  # Replace with the desired output file name
+
+        context = {'staff':staff, 'output_file': output_file}
+        generate_qr_code(my_data, output_file)
+        # return render(request, 'AttendanceApp/visitor.html', context)
         return redirect('/visitor')
-    return render(request, 'AttendanceApp/visitor.html')
+
+    context = {'staff':staff}
+    return render(request, 'AttendanceApp/visitor.html', context)
 
 
 def generate_qr_code(reference_id, output_file):
