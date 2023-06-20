b96f82b13cd4e8e061a617f127cca6739e837e6b
DECODER69 <rejaymorada0@gmail.com>
2023-06-20 14:25:47 +0800

    Add Files

--- AttendanceProject/AttendanceApp/models.py
+++ AttendanceProject/AttendanceApp/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.db.models import Model
 import os
+from django.core.validators import RegexValidator
 
 class Visitors(models.Model):
+    cellphone_regex = RegexValidator(
+        regex=r'^09\d{9}$',
+        message="Enter a valid cellphone number."
+    )
+    
     rfid = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=255, null=True, blank=True)
     last_name = models.CharField(max_length=255, null=True, blank=True)
-    contact_number = models.IntegerField(null=True, blank=True)
+    contact_number = models.CharField(max_length=11, validators=[cellphone_regex])
     person_to_visit = models.ForeignKey("Staffs", on_delete=models.CASCADE)
     purpose = models.CharField(max_length=500, null=True, blank=True)
     picture = models.FileField(upload_to ='pictures/', null=True, blank=True)
